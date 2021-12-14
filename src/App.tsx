c13f5c24c7cc58dce4c6b23141252014ba0bc44b
F0gr1 <gami20010314@gmail.com>
2021-12-15 00:08:23 +0900

    MyAvararコンポーネント追加’

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import MyAvatar from './components/MyAvatar'
 const App: React.FC = () => {
   return (
     // Navbarコンポーネントを追記
+    <>
     <Navbar />
+    <MyAvatar />
+    </>
   );
 }
 
