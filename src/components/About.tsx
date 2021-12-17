a7c61f77d7363191bce3f365d366fde3afaa6e96
F0gr1 <gami20010314@gmail.com>
2021-12-17 23:46:52 +0900

    About を追加

---
+++ src/components/About.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+const About: React.FC = () => {
+  return (
+    <>
+      <Box p={2}>
+        <Box display='flex' justifyContent='center' p={1}>
+          <Typography variant='h5' >
+            About
+          </Typography>
+        </Box>
+        <Box display='flex' justifyContent='center' p={1}>
+          <Typography variant='body1' align='left'>
+            私の名前は石神勇貴です。<br />
+            好きな言語・フレームワークはTypeScriptとReactです。
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default About;
