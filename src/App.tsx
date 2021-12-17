7dc50f54c12ac85ca680a8b58189c7d1edcda5a3
F0gr1 <gami20010314@gmail.com>
2021-12-17 23:47:55 +0900

    Componentをコンポーネントを追記

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import MyAvatar from './components/MyAvatar'
+import About from './components/About'
+import Skill from './components/Skills'
+import Works from './components/Works'
 const App: React.FC = () => {
   return (
     // Navbarコンポーネントを追記
     <>
     <Navbar />
     <MyAvatar />
+    <About />
+    <Skill />
+    <Works />
     </>
   );
 }
