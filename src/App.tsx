fc79ca19101226b7e116e0e29e5790d91d5c8d65
F0gr1 <gami20010314@gmail.com>
2021-12-20 17:14:22 +0900

    スクロールアップなりやpage遷移

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,35 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import MyAvatar from './components/MyAvatar'
 import About from './components/About'
-import Skill from './components/Skills'
+import Skills from './components/Skills'
 import Works from './components/Works'
 import Contact from './components/Contact'
+// 追記
+import ScrollUp from './utils/ScrollUp'
+
 const App: React.FC = () => {
   return (
-    // Navbarコンポーネントを追記
     <>
-    <Navbar />
-    <MyAvatar />
-    <About />
-    <Skill />
-    <Works />
-    <Contact/>
+      <section>
+        <Navbar />
+      </section>
+      <section>
+        <MyAvatar />
+      </section>
+      <section id='about'>
+        <About />
+      </section>
+      <section id='skills'>
+        <Skills />
+      </section>
+      <section id='works'>
+        <Works />
+      </section>
+      <section id='contact'>
+        <Contact />
+      </section>
+      // 追記
+      <ScrollUp />
     </>
   );
 }
